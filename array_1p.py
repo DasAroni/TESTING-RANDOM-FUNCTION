df49caacd708735028dbfb841d040d5d14c81fbf
DasAroni <107314887+DasAroni@users.noreply.github.com>
2023-02-08 19:15:58 +0530

    Add files via upload

---
+++ array_1p.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+root = Tk()
+root.title("TESTING RANDOM FUNCTION")
+root.geometry("500x500")
+root.configure(background="yellow")
+
+entry_1 = Entry(root)
+label_1 = Label(root)
+label_2 = Label(root)
+
+
+
+root.mainloop()
